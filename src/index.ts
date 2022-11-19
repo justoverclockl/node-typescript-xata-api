6b8e665ad693f51fe94679a947a4b6eed8ca3e3e
Marco Colia <info@justoverclock.it>
2022-11-19 18:47:06 +0100

    basic boilerplate

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world')
+import express, {Express, Request, Response } from "express";
+import dotenv from 'dotenv';
+
+const app:Express = express();
+const PORT = process.env.PORT || 3050;
+
+app.get('/api/jobs', (req: Request, res: Response) => {
+
+})
+
+app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`))
